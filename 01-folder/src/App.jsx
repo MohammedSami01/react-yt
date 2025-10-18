b6f47155d0413ff4a60486c67522d51943eaf1d7
Your Name <your.email@example.com>
2025-10-18 22:15:10 +0530

    folder-01

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <h1>React Vite App</h1>
+      <p>This is a simple React application using Vite as the build tool.</p>
+    </>
+  )
+}
+
+export default App
