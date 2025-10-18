0b65578b213b26992c1d33b148509064fe674462
Your Name <your.email@example.com>
2025-10-18 23:59:54 +0530

    Add 02-folder with React Vite setup

---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return (
+        <div className="card">
+            <h1>Sami</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit tenetur porro doloribus animi sed. Eveniet repudiandae quo adipisci doloremque, ab ut quaerat distinctio placeat sint nam veniam rem quae expedita.</p>
+        </div>
+    )
+}
+
+
+export default Card;
