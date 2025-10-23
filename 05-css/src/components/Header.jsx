d8737d151d9863b590c104a051cb29a3180fb7fc
MohammedSami01 <itzsamii6980@gmail.com>
2025-10-23 12:43:51 +0530

    css

---
+++ 05-css/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+    <h3>This is an Header Page Demo</h3>
+    <button className='btn'>Click Me</button>
+        </div>
+  )
+}
+
+export default Header
